9ecad460cb0bd5c018d3c7708453c1450d4d549d
nigel7huyc <nigel7huyc@163.com>
2021-08-05 10:33:36 +0800

    [ADD] Intelligence Search Function

--- analysis/cores/vt_intelligence.py
+++ analysis/cores/vt_intelligence.py
@@ -21,10 +21,11 @@ class IntelligenceHandler:
         """
         api_flag = 1
         ruleset_id_dict = {}
+        params_dict = self.extra_params
         api_key = self.vt_tools.get_api(api_flag)
         query_url = os.path.join(self.url_prefix, "hunting_rulesets")
         with vt.Client(api_key, trust_env=True) as client:
-            ruleset_json = client.get_json(query_url, params=self.extra_params)
+            ruleset_json = client.get_json(query_url, params=params_dict)
         if "data" in ruleset_json:
             json_data = ruleset_json["data"]
             if "cursor" in ruleset_json["meta"]:
@@ -42,7 +43,7 @@ class IntelligenceHandler:
         return ruleset_id_dict
 
     @staticmethod
-    def remove_duplicate_files(input_data):
+    def remove_duplicate_id(input_data):
         [id_list, output_data] = [[], []]
         for file_element in input_data:
             id_value = file_element["id"]
@@ -60,10 +61,11 @@ class IntelligenceHandler:
         :rtype: list
         """
         api_flag = 1
+        params_dict = self.extra_params
         api_key = self.vt_tools.get_api(api_flag)
         query_url = os.path.join(self.url_prefix, "hunting_rulesets", ruleset_id, "hunting_notification_files")
         with vt.Client(api_key, trust_env=True) as client:
-            notified_files_json = client.get_json(query_url, params=self.extra_params)
+            notified_files_json = client.get_json(query_url, params=params_dict)
         if "data" in notified_files_json:
             json_data = notified_files_json["data"]
             if "cursor" in notified_files_json["meta"]:
@@ -73,5 +75,29 @@ class IntelligenceHandler:
         else:
             logger.error("[get_notification_files] Query Failed, Error Message >> {}".format(notified_files_json))
             return QUERY_FAILED
-        distinct_notifications = self.remove_duplicate_files(json_data)
+        distinct_notifications = self.remove_duplicate_id(json_data)
         return distinct_notifications
+
+    def get_search_result(self, input_params):
+        api_flag = 1
+        params_dict = self.extra_params
+        params_dict["limit"] = input_params.get("limit", 300)
+        params_dict["query"] = input_params.get("query")
+        params_dict["order"] = input_params.get("order")
+        for key_value in params_dict:
+            if params_dict["key_value"] is None:
+                del params_dict[key_value]
+        logger.info("[get_search_result] The Input Parameters is {}".format(params_dict))
+        api_key = self.vt_tools.get_api(api_flag)
+        query_url = os.path.join(self.url_prefix, "search")
+        try:
+            with vt.Client(api_key, trust_env=True) as client:
+                search_results = client.get_json(query_url, params=params_dict)
+        except Exception as e:
+            logger.error("[get_search_result] Query Failed, Error Message >> {}".format(e))
+            return QUERY_FAILED
+        json_data = search_results.get("data")
+        distinct_search_results = self.remove_duplicate_id(json_data)
+        return distinct_search_results
+
+
--- analysis/main.py
+++ analysis/main.py
@@ -24,10 +24,10 @@ def get_health_status():
     return jsonify(response)
 
 
-@app.route('/v3/hunting/rules_info', methods=['GET'])
+@app.route('/v3/intelligence/rules_info', methods=['GET'])
 def rules_info():
-    live_hunter = IntelligenceHandler()
-    rules_dict = live_hunter.get_ruleset_id()
+    intelligence_handler = IntelligenceHandler()
+    rules_dict = intelligence_handler.get_ruleset_id()
     if type(rules_dict) is int or rules_dict is None:
         response = error_msg(rules_dict)
         response["data"] = {}
@@ -37,20 +37,36 @@ def rules_info():
     return jsonify(response)
 
 
-@app.route("/v3/hunting/notification_info", methods=["POST"])
+@app.route("/v3/intelligence/notification_info", methods=["POST"])
 def notification_info():
     params = request.json
-    live_hunter = IntelligenceHandler()
+    intelligence_handler = IntelligenceHandler()
     rule_id_value = params["rules_id"]
     save_path = "hunting_notifications/{}_notifications.json".format(rule_id_value[:10])
     logger.info("[notification_info] The Rules ID is {}".format(rule_id_value))
-    notifications_data = live_hunter.get_notification_files(rule_id_value)
+    notifications_data = intelligence_handler.get_notification_files(rule_id_value)
     if type(notifications_data) is int:
         response = error_msg(notifications_data)
     else:
         response = error_msg(SUCCESS_CODE)
         store_jsonfile(save_path, notifications_data)
         logger.info("[notification_info] Store Notification Data into {}".format(save_path))
+    return jsonify(response)
+
+
+@app.route("/v3/intelligence/notification_info", methods=["POST"])
+def intelligence_search():
+    params = request.json
+    query_info = params.get("query")
+    intelligence_handler = IntelligenceHandler()
+    save_path = "search_results/{}_search.json".format("_".join(query_info.split(" ")))
+    search_results = intelligence_handler.get_search_result(params)
+    if type(search_results) is int:
+        response = error_msg(search_results)
+    else:
+        response = error_msg(SUCCESS_CODE)
+        store_jsonfile(save_path, search_results)
+        logger.info("[intelligence_search] Store Notification Data into {}".format(save_path))
     return jsonify(response)
 
 
