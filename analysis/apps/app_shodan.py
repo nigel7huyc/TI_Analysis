4aa40f17e0a292938fac86a0bd52aee2542c602c
nigel7huyc <nigel7huyc@163.com>
2021-08-12 11:50:15 +0800

    [MOD] shodan_app

--- analysis/apps/app_shodan.py
+++ analysis/apps/app_shodan.py
@@ -1,12 +1,97 @@
 from datetime import datetime
-from flask import Flask, jsonify, request, Blueprint
+from flask import jsonify, request, Blueprint
 
 from settings import *
-from cores.vt_intelligence import IntelligenceHandler
-from cores.vt_files import FileHandler
+from cores.shodan_explore import ShodanHandler
 from utils.utils_log import LogFactory
 
 logger = LogFactory.get_log("audit")
 
 shodan_app = Blueprint("shodan", __name__)
+
+
+@shodan_app.route("/explore_host", methods=["POST"])
+def get_ip_info():
+    """ get specific host information with shodan explore
+
+    @@@
+    ### args
+    |  args | nullable | request type | type |  remarks |
+    |-------|----------|--------------|------|----------|
+    |  host |  false   |    body     |  str  | the ip address of hosts |
+    | history | true   |    body     | 0 / 1 | grab the historical banners for the host, false otherwise |
+    | minify | true    |    body     | 0 / 1 | return list of ports and general host information, false otherwise |
+
+
+    ### request
+    ```
+    http://127.0.0.1:5000/shodan/explore_host
+    ```
+
+    ### return
+    ```json
+    {"code": "0", "message": "SUCCESS"}
+    ```
+
+    ### Output
+    * Output_Dir: `output/shodan/`
+    * Filename: `host_{$FILE_ID}.json`
+    @@@
+    """
+    the_params = request.json
+    shodan_handler = ShodanHandler()
+    store_path = "shodan/host_{}.json".format(the_params["host"])
+    ip_info = shodan_handler.shodan_host(the_params)
+    if type(ip_info) is int:
+        response = error_msg(ip_info)
+    else:
+        response = error_msg(SUCCESS_CODE)
+        store_jsonfile(store_path, ip_info)
+    return jsonify(response)
+
+
+@shodan_app.route("/explore_search", methods=['POST'])
+def get_shodan_search():
+    """ get specific host information with shodan explore
+
+    @@@
+    ### args
+    |  args | nullable | request type | type |  remarks |
+    |-------|----------|--------------|------|----------|
+    | query |  false   |    body     |  str  | Search query |
+    | page |  true  | body | int | Page number of the search results |
+    | limit | true | body | int | Number of results to return |
+    | offset | true | body | int | Search offset to begin getting results from |
+    | facets | true | body | int | A list of properties to get summary information on |
+    | minify | true | body | int | Whether to minify the banner and only return the important data |
+
+
+    ### request
+    ```
+    http://127.0.0.1:5000/shodan/explore_search
+    ```
+
+    ### return
+    ```json
+    {"code": "0", "message": "SUCCESS"}
+    ```
+
+    ### Output
+    * Output_Dir: `output/shodan/`
+    * Filename: `search_{$FILE_ID}.json`
+    @@@
+    """
+    today = datetime.now()
+    the_params = request.json
+    shodan_handler = ShodanHandler()
+    tag_value = the_params["query"].split(" ")[0]
+    dt_string = today.strftime("%d_%m_%Y-%H:%M:%S")
+    store_path = "shodan/search_{}_{}".format(tag_value, dt_string)
+    search_result = shodan_handler.shodan_search(the_params)
+    if type(search_result) is int:
+        response = error_msg(search_result)
+    else:
+        response = error_msg(SUCCESS_CODE)
+        store_jsonfile(store_path, search_result)
+    return jsonify(response)
 
--- analysis/cores/shodan_explore.py
+++ analysis/cores/shodan_explore.py
@@ -11,20 +11,31 @@ class ShodanHandler:
         api_key = shodan_api_key
         self.shodan_api = shodan.Shodan(api_key, proxies=proxies_host)
 
-    def shodan_host(self, host_name: str):
+    def shodan_host(self, input_params: dict):
+        host_name = input_params.get("host")
+        history_value = input_params.get("history", 0)
+        minify_value = input_params.get("minify", 0)
         try:
-            host_info = self.shodan_api.host(host_name)
+            host_info = self.shodan_api.host(host_name, history_value, minify_value)
         except Exception as e:
             logger.error("[shodan_host] Shodan Query Host '{}' Information Error, Details: {}".format(host_name, e))
             return QUERY_FAILED
         logger.info("[shodan_host] Queried the Information for the host '{}'".format(host_name))
         return host_info
 
-    def shodan_search(self, query_str):
+    def shodan_search(self, input_params: dict):
+        query_value = input_params.get("query")
+        page_value = input_params.get("page", 1)
+        limit_value = input_params.get("limit")
+        offset_value = input_params.get("offset")
+        facets_value = input_params.get("facets")
+        minify_value = input_params.get("minify")
         try:
-            search_results = self.shodan_api.search(query_str)
+            search_results = self.shodan_api.search(query_value, page_value, limit_value, offset_value, facets_value,
+                                                    minify_value)
         except Exception as e:
-            logger.error("[shodan_search] Shodan Search with Input Query '{}' Failed, Details: {}".format(query_str, e))
+            logger.error(
+                "[shodan_search] Shodan Search with Input Query '{}' Failed, Details: {}".format(query_value, e))
             return QUERY_FAILED
-        logger.info("[shodan_search] Searched the Results with the Input Query '{}'".format(query_str))
+        logger.info("[shodan_search] Searched the Results with the Input Query '{}'".format(query_value))
         return search_results
